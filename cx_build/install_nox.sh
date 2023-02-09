113dda5c4b7a2bf78bfec7ab8edcb07472fd3b4d
femanov <femanov@gmail.com>
2023-02-09 22:19:24 +0700

    build scripts corrected

--- cx_build/install_nox.sh
+++ cx_build/install_nox.sh
@@ -7,9 +7,9 @@ CXDIR=$HOME/cx
 
 CXINSTDIR=$HOME/4pult
 
-make -C $CXDIR/4cx/src install PREFIX=$CXINSTDIR
+make -C $CXDIR/4cx/src install PREFIX=$CXINSTDIR NOX=1
 
-make -C $CXDIR/frgn4cx install PREFIX=$CXINSTDIR EPICS_BASE_DIR=$HOME/epics/base
+make -C $CXDIR/frgn4cx install PREFIX=$CXINSTDIR EPICS_BASE_DIR=$HOME/epics/base NOX=1
 
 
 
--- cx_build/make_all_nox.sh
+++ cx_build/make_all_nox.sh
@@ -12,6 +12,6 @@ make -C $CXDIR/4cx/src NOX=1
 make -C $CXDIR/4cx/src exports NOX=1
 
 make -C $CXDIR/4cx/src exports create-gitignore
-make -C $CXDIR/frgn4cx EPICS_BASE_DIR=$HOME/epics/base
+make -C $CXDIR/frgn4cx EPICS_BASE_DIR=$HOME/epics/base NOX=1
 
 make -C $CXDIR/frgn4cx EPICS_BASE_DIR=$HOME/epics/base create-gitignore
